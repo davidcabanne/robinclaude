3318b0bc3704420d0ba721053fcce5e68fc0ed09
davidcabanne <davidcbne@gmail.com>
2023-11-02 20:22:48 +0100

    fixed: spacing on post pages

--- frontend/components/Content.js
+++ frontend/components/Content.js
@@ -3,10 +3,20 @@ import * as _var from "../styles/variables";
 
 const Container = styled.div`
   width: 100%;
-  margin-top: ${_var.marginXL};
+  margin-top: ${_var.marginL};
   display: flex;
   flex-direction: column;
   gap: ${_var.marginS};
+
+  @media ${_var.device.tablet_max} {
+    margin-top: ${_var.marginL};
+  }
+  @media ${_var.device.mobileL_max} {
+    margin-top: ${_var.marginM};
+  }
+  @media ${_var.device.mobileS_max} {
+    margin-top: ${_var.marginS};
+  }
 `;
 
 export default function Content({ children }) {
